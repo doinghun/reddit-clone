fd9b584d31ae11513fff5e589514b4c749a3124b
Dohun Kim <de.qtner@gmail.com>
2021-02-03 02:37:04 +0800

    Process API response for PostList & Post screens

--- src/state/Post/api.js
+++ src/state/Post/api.js
@@ -1,5 +1,18 @@
 import axios from 'util/axios';
 
 export function getPosts(category) {
-  return axios.get('r/' + category + '.json?limit=25');
+  return axios
+    .get('r/' + category + '.json?limit=25')
+    .then((res) => {
+      if (res.status >= 200 && res.status < 300) {
+        return res.data.data.children.map(
+          ({
+            data: { title, author, num_comments, selftext, id, score, created },
+          }) => ({ title, author, num_comments, selftext, id, score, created })
+        );
+      }
+    })
+    .catch((error) => {
+      return Promise.reject(error);
+    });
 }
