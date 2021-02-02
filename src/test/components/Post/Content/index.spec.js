f900dc4925dd1fb8b50cc0e37863837d0a84428c
Dohun Kim <de.qtner@gmail.com>
2021-02-02 22:43:56 +0800

    Update PostContent test

---
+++ src/test/components/Post/Content/index.spec.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import PC from 'components/Post/Content';
+import PostContentDetail from 'components/Post/Content/Details';
+import PostContentComments from 'components/Post/Content/Comments';
+
+describe('PostContent', () => {
+  const PostContent = shallow(<PC />);
+  it('Should render correctly', () => {
+    expect(PostContent.find(PostContentDetail).exists()).toBeTruthy();
+    expect(PostContent.find(PostContentComments).exists()).toBeTruthy();
+  });
+});
