1e3377eb5684dca7548aed3ed73b0d690a5da580
Dohun Kim <de.qtner@gmail.com>
2021-02-06 02:13:35 +0800

    Update styles to Vote Component depending on score

--- src/components/Post/Vote/index.js
+++ src/components/Post/Vote/index.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { kFormatter } from 'util/number';
-import { PostVoteContainer, PostVoteButton } from './styledComponents';
+import {
+  PostVoteContainer,
+  PostUpVoteButton,
+  PostDownVoteButton,
+  PostVoteScore,
+} from './styledComponents';
 import { ImArrowUp, ImArrowDown } from 'react-icons/im';
 import { increaseScore, decreaseScore } from 'state/Post/action';
 
-const PostVote = (props) => (
-  <PostVoteContainer>
-    <PostVoteButton onClick={() => props.increaseScore(props.id)}>
-      <ImArrowUp size={16} />
-    </PostVoteButton>
-    <span>{kFormatter(props.score)}</span>
-    <span>{props.local_score}</span>
-    <PostVoteButton onClick={() => props.decreaseScore(props.id)}>
-      <ImArrowDown size={16} />
-    </PostVoteButton>
-  </PostVoteContainer>
-);
+const PostVote = (props) => {
+  const {
+    increaseScore,
+    decreaseScore,
+    id,
+    local_score,
+    upVoted,
+    downVoted,
+  } = props;
+
+  return (
+    <PostVoteContainer>
+      <PostUpVoteButton onClick={() => increaseScore(id)} upVoted={upVoted}>
+        <ImArrowUp size={16} />
+      </PostUpVoteButton>
+      <PostVoteScore upVoted={upVoted} downVoted={downVoted}>
+        {local_score}
+      </PostVoteScore>
+      <PostDownVoteButton onClick={() => decreaseScore(id)} downVoted={downVoted}>
+        <ImArrowDown size={16} />
+      </PostDownVoteButton>
+    </PostVoteContainer>
+  );
+};
 
 export default connect(null, { increaseScore, decreaseScore })(PostVote);
--- src/components/Post/Vote/styledComponents.js
+++ src/components/Post/Vote/styledComponents.js
@@ -2,15 +2,22 @@ import styled from 'styled-components/macro';
 
 export const PostVoteButton = styled.button`
   border: 0;
-  border-radius: 3px;
   height: 22px;
   width: 22px;
   background-color: transparent;
   cursor: default;
-
+  outline: none;
   span {
     width: 100%;
   }
+`;
+
+export const PostUpVoteButton = styled(PostVoteButton)`
+  color: ${(props) => (props.upVoted ? props.theme.upvote : '#000000')};
+`;
+
+export const PostDownVoteButton = styled(PostVoteButton)`
+  color: ${(props) => (props.downVoted ? props.theme.downvote : '#000000')};
 `;
 
 export const PostVoteContainer = styled.div`
@@ -24,4 +31,15 @@ export const PostVoteContainer = styled.div`
   line-height: 25px;
   font-weight: 500;
   text-align: center;
+  border-radius: 4px;
+  background: #f8f9fa;
+`;
+
+export const PostVoteScore = styled.span`
+  color: ${(props) =>
+    props.upVoted
+      ? props.theme.upvote
+      : props.downVoted
+      ? props.theme.downvote
+      : '#000000'};
 `;
