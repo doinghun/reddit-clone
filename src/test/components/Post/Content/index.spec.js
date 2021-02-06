fbff2fdb96cea985faea5bd1f77962c35dd6524b
Dohun Kim <de.qtner@gmail.com>
2021-02-06 17:15:39 +0800

    Add test for PostContent component

--- src/test/components/Post/Content/index.spec.js
+++ src/test/components/Post/Content/index.spec.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import PC from 'components/Post/Content';
+import PostContent from 'components/Post/Content';
+import PostContentInfo from 'components/Post/Content/Info';
+import PostContentTitle from 'components/Post/Content/Title';
 import PostContentDetail from 'components/Post/Content/Detail';
 import PostContentComments from 'components/Post/Content/Comments';
 
 describe('PostContent', () => {
-  const PostContent = shallow(<PC />);
+  const wrapper = shallow(<PostContent />);
   it('Should render correctly', () => {
-    expect(PostContent.find(PostContentDetail).exists()).toBeTruthy();
-    expect(PostContent.find(PostContentComments).exists()).toBeTruthy();
+    expect(wrapper.find(PostContentInfo).exists()).toBeTruthy();
+    expect(wrapper.find(PostContentTitle).exists()).toBeTruthy();
+    expect(wrapper.find(PostContentDetail).exists()).toBeTruthy();
+    expect(wrapper.find(PostContentComments).exists()).toBeTruthy();
   });
 });
