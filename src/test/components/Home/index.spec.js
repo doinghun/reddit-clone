abb3c03110a97eff5589618f1920b6dedf1f261a
Dohun Kim <de.qtner@gmail.com>
2021-02-06 19:38:32 +0800

    Update test for Home component

--- src/test/components/Home/index.spec.js
+++ src/test/components/Home/index.spec.js
@@ -2,7 +2,15 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Home from 'components/Home';
+import {
+  HomeMainSection,
+  SubRedditTitle,
+} from 'components/Home/styledComponents';
 
-it('Should render correctly', () => {
-  shallow(<Home />);
+describe('Home', () => {
+  const wrapper = shallow(<Home />);
+  it('Should render correctly', () => {
+    expect(wrapper.find(HomeMainSection).exists()).toBeTruthy();
+    expect(wrapper.find(SubRedditTitle).exists()).toBeTruthy();
+  });
 });
