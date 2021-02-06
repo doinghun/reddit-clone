e9916ca8aa4dfe52d9c07c9b751f66770dd07398
Dohun Kim <de.qtner@gmail.com>
2021-02-06 10:51:22 +0800

    Update test for App

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,23 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+global.IntersectionObserver = class IntersectionObserver {
+  constructor() {}
+
+  disconnect() {
+    return null;
+  }
+
+  observe() {
+    return null;
+  }
+
+  takeRecords() {
+    return null;
+  }
+
+  unobserve() {
+    return null;
+  }
+};
 
 Enzyme.configure({ adapter: new Adapter() });
--- src/test/App.spec.js
+++ src/test/App.spec.js
@@ -1,9 +1,37 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../App';
+import { MemoryRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { mount } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import { ThemeProvider } from 'styled-components';
+import theme from 'theme';
+import App from '../App.js';
 
 describe('App', () => {
   it('Should render correctly', () => {
-    shallow(<App />);
+    const initialState = {
+      posts: {
+        view: 'card',
+        posts: [],
+      },
+    };
+    const middlewares = [thunk];
+
+    const mockStore = configureStore(middlewares);
+    const store = mockStore(() => initialState);
+    const app = mount(
+      <Provider store={store}>
+        <ThemeProvider theme={theme('card')}>
+          <MemoryRouter>
+            <App />
+          </MemoryRouter>
+        </ThemeProvider>
+      </Provider>
+    );
+    const switches = app.find(Switch);
+    expect(switches).toHaveLength(1);
+    const routes = app.find(Route);
+    expect(routes).toHaveLength(1);
   });
 });
